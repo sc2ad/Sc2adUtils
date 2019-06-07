05e7de76c02d9885b7dad0981b951744d2618bad
Adam ? <adamznow@gmail.com>
2019-06-07 16:52:24 -0700

    Added functional validation, fixed schema, provided template.json

--- validate_config.py
+++ validate_config.py
@@ -3,6 +3,10 @@ import argparse
 from jsonschema import validate
 
 def validateJson(js, schema):
+    """
+    Confirms that a given json follows the given schema.
+    Throws a "jsonschema.excpetions.ValidationError" otherwise.
+    """
     assert type(js) == dict, "JSON must be a dictionary!"
     assert type(schema) == dict, "Schema must be a dictionary!"
     validate(instance=js, schema=schema)
@@ -12,14 +16,18 @@ def loadJson(string):
         return json.loads(string)
     except json.JSONDecodeError:
         # This is fine, just need to attempt to load as file
-        return json.load(args.config)
+        with open(string, 'r') as f:
+            return json.load(f)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Reads the given config json file and confirms the config schema")
-    parser.add_argument("config", required=True, help="The JSON config string or file to validate")
-    parser.add_argument("schema", required=False, default="template_lazy.json", help="The JSON schema to follow")
+    parser.add_argument("config", help="The JSON config string or file to validate")
+    parser.add_argument("--schema", default="template_lazy.json", help="The JSON schema to follow")
 
     args = parser.parse_args()
     
     js = loadJson(args.config)
     schema = loadJson(args.schema)
+
+    # Throws an error if the JSON is not in the proper format
+    validateJson(js, schema)
