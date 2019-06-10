20fdd90f0558c9c00e96e325c2e97757d3da1933
Adam ? <adamznow@gmail.com>
2019-06-10 11:04:18 -0700

    Supports YAML and JSON validation, auto converts tuples to lists
    Still want to go over and confirm that all the constraints are properly set. More defensive programming, and also need to make sure that load is the only method called.

--- validate_config.py
+++ validate_config.py
@@ -1,6 +1,16 @@
 import json
 import argparse
-from jsonschema import validate
+try:
+    # python -m pip install jsonschema
+    from jsonschema import validate
+except:
+    raise ImportError("You must install json schema via pip in order to use this validator!")
+# python -m pip install pyyaml
+try:
+    import yaml
+    YAML = True
+except ImportError:
+    YAML = False
 
 MEMOIZE = False
 
@@ -13,6 +23,36 @@ def validateJson(js, schema):
     assert type(schema) == dict or type(schema) == str, "Schema must be a dictionary or string JSON or path!"
     validate(instance=loadJson(js), schema=loadJson(schema))
 
+def validateYaml(yaml, schema):
+    """
+    Confirms that a given yaml follows the given schema.
+    The schema can be a json string/dict.
+    Throws a "jsonschema.exceptions.ValidationError" otherwise.
+    """
+    assert YAML, "Must install PyYAML before attempting to load yaml files!"
+    assert type(yaml) == dict or type(yaml) == str, "YAML must be a dictionary or path to a YAML file!"
+    assert type(schema) == dict or type(schema) == str, "Schema must be a dictionary or string JSON or path to schema.json!"
+    validate(instance=loadYaml(yaml), schema=loadJson(schema))
+
+def fixTuples(data):
+    for key in data.keys():
+        if type(data[key]) == tuple:
+            data[key] = list(data[key])
+        if type(data[key]) == dict:
+            data[key] = fixTuples(data[key])
+    return data
+
+def isJson(data):
+    return data.endswith("}") or data.endswith(".json")
+
+def validateData(data, schema):
+    if isJson(data):
+        # JSON
+        validateJson(data, schema)
+    else:
+        # YAML
+        validateYaml(data, schema)
+
 def loadAndValidateJson(js, schema):
     """
     Loads the JSON config from the given js and schema strings, dicts, or paths
@@ -20,6 +60,24 @@ def loadAndValidateJson(js, schema):
     """
     validateJson(js, schema)
     return loadJson(js)
+
+def loadAndValidateYaml(yaml, schema):
+    """
+    Loads the YAML config from the given yaml and schema strings, dicts, or paths
+    And returns the YAML if valid. Otherwise, it will throw a "jsonschema.exceptions.ValidationError".
+    """
+    validateYaml(yaml, schema)
+    return loadYaml(yaml)
+
+def load(data, schema):
+    """
+    Loads the given data and validates it according to the schema provided.
+    Data must be either JSON or YAML, it must be a dictionary, a path, or a string of JSON.
+    Schema must be JSON, it must be a dictionary, a path, or a string of JSON.
+    """
+    if isJson(data):
+        return loadAndValidateJson(data)
+    return loadAndValidateYaml(data)
 
 def memoize(f):
     m = {}
@@ -42,12 +100,19 @@ def loadJson(data):
         with open(data, 'r') as f:
             return json.load(f)
 
+@memoize
+def loadYaml(data):
+    if type(data) == dict:
+        return data
+    with open(data, 'r') as stream:
+        return fixTuples(yaml.load(stream, Loader=yaml.FullLoader))
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Reads the given config json file and confirms the config schema")
-    parser.add_argument("config", help="The JSON config string or file to validate")
+    parser.add_argument("config", help="The JSON or YAML config string or file to validate")
     parser.add_argument("--schema", default="template_schema.json", help="The JSON schema to follow or the path to the schema")
 
     args = parser.parse_args()
     
     # Throws an error if the JSON is not in the proper format
-    validateJson(args.config, args.schema)
+    validateData(args.config, args.schema)
